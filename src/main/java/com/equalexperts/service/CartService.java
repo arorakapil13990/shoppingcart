ba1fb290d790a009b5ab96518147848e4ff83b67
Your Name <you@example.com>
2019-09-29 18:17:01 +0530

    Commit 5

--- src/main/java/com/equalexperts/service/CartService.java
+++ src/main/java/com/equalexperts/service/CartService.java
@@ -17,13 +17,13 @@ public class CartService {
     private Map<User, Cart> userCartMap = new HashMap<>();
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.00");
 
-	void addItem(User user, Cart cart) {
-		userCartMap.computeIfAbsent(user, k -> cart);
-	}
+    void addItem(User user, Cart cart) {
+        userCartMap.computeIfAbsent(user, k -> cart);
+    }
 
-    
+
     double getTotalPriceForUser(User user) {
-        return parseDouble(DECIMAL_FORMAT.format(userCartMap.get(user).getItems().stream().mapToDouble(product -> product.getQuantity() * product.getPrice()).sum()));
+        return parseDouble(DECIMAL_FORMAT.format(user.getCart().getItems().stream().mapToDouble(cart -> cart.getQuantity() * cart.getProduct().getPrice()).sum()));
     }
 
 
